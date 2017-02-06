ae1bbe097bef7756da69a0441926bc88063e4048
Anya Keller <anyaisanya@gmail.com>
2017-02-06 14:08:31 -0500

    did recent hw

--- readMongo.py
+++ readMongo.py
@@ -2,6 +2,16 @@ from pymongo import MongoClient
 
 def getData():
     c = MongoClient("lisa.stuy.edu",27017)
-    print MongoClient.find()
+    db = c["meme-team-lite"]
+    studentsInfo = db.students.find()
+    for student in studentsInfo:
+	total = 0
+	numClasses = 0
+	for course in student["classes"]:
+	    total += int(course["mark"])
+	    numClasses += 1
+	print "Student Name: " + student["name"]
+        print "Average: " 
+	print (total/numClasses)   
 
 getData()
